c8d1f4b2424b82dd995c1f920394ad9a54c7a03d
gitstartednow <112145540+gitstartednow@users.noreply.github.com>
2024-10-19 15:42:35 -0400

    Add SwiftLint
    Add SwiftLint

--- demo/demo/demoApp.swift
+++ demo/demo/demoApp.swift
@@ -1,17 +1,1 @@
-//
-//  demoApp.swift
-//  demo
-//
-//  Created by Don Mallow on 10/19/24.
-//
 
-import SwiftUI
-
-@main
-struct demoApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
